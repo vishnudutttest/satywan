a98a78f5ec110ae829e1f719445319aa02624731
Vishnu Dutt <vishnudutttest@gmail.com>
2022-10-21 17:07:27 +0530

    Dashboard report
    updated report on dashboard

--- frontend/src/components/Report/DashboardReport.js
+++ frontend/src/components/Report/DashboardReport.js
@@ -2,14 +2,14 @@ const DashboardReport =()=>{
     return (<>
     <h2>Reports</h2>
     <div>
-        <div class="flex justify-left">
-            <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
-                <div class="py-3 px-6 border-b border-gray-300">
+        <div className="flex justify-left">
+            <div className="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+                <div className="py-3 px-6 border-b border-gray-300">
                 Today (2022-10-21)
                 </div>
-                <div class="p-6">
-                <h5 class="text-gray-900 text-xl font-medium mb-2"> </h5>
-                <p class="text-gray-700 text-base mb-4">
+                <div className="p-6">
+                <h5 className="text-gray-900 text-xl font-medium mb-2"> </h5>
+                <p className="text-gray-700 text-base mb-4">
             
                     Collection Amount: Rs 10000 <br />
                     Collection Amount: Rs 9500
@@ -19,14 +19,14 @@ const DashboardReport =()=>{
             
             </div>
         </div>
-        <div class="flex justify-center">
-            <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
-                <div class="py-3 px-6 border-b border-gray-300">
+        <div className="flex justify-center">
+            <div className="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+                <div className="py-3 px-6 border-b border-gray-300">
                 Week (Mon-Sun)
                 </div>
-                <div class="p-6">
-                <h5 class="text-gray-900 text-xl font-medium mb-2"> </h5>
-                <p class="text-gray-700 text-base mb-4">
+                <div className="p-6">
+                <h5 className="text-gray-900 text-xl font-medium mb-2"> </h5>
+                <p className="text-gray-700 text-base mb-4">
             
                     Collection Amount: Rs 10000 <br />
                     Collection Amount: Rs 9500
@@ -36,14 +36,14 @@ const DashboardReport =()=>{
             
             </div>
         </div>
-        <div class="flex justify-right">
-            <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
-                <div class="py-3 px-6 border-b border-gray-300">
+        <div className="flex justify-right">
+            <div className="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+                <div className="py-3 px-6 border-b border-gray-300">
                 Month (1st-31st)
                 </div>
-                <div class="p-6">
-                <h5 class="text-gray-900 text-xl font-medium mb-2"> </h5>
-                <p class="text-gray-700 text-base mb-4">
+                <div className="p-6">
+                <h5 className="text-gray-900 text-xl font-medium mb-2"> </h5>
+                <p className="text-gray-700 text-base mb-4">
             
                     Collection Amount: Rs 10000 <br />
                     Collection Amount: Rs 9500
